a3013550460fe8b52210713baf76828e13792107
paw1470 <pawel1470@yahoo.com>
2020-02-12 18:25:29 +0100

    Add example and  PlatformIO project example

---
+++ example/example.ino
@@ -0,0 +1,21 @@
+#include "ButtonPB.h"
+
+#define BUTTON_PIN 2
+#define LED_PIN 13
+
+Button button;
+
+void setup(){
+    pinMode(LED_PIN, OUTPUT);
+    button.init(BUTTON_PIN); //default argument = true, button with internal pull up resistor
+    // button.init(BUTTON_PIN, true); //button with internal pull up resistor
+    //button.init(BUTTON_PIN, false); //you need to add external pull up resistor. Default is 10k between VCC and PIN
+}
+
+void loop(){
+    if(button.isButtonPressed()){
+        digitalWrite(LED_PIN, HIGH);
+    } else{
+        digitalWrite(LED_PIN, LOW);
+    }
+}
